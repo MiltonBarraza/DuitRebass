71f92d956d69a0787d7242f59e556480c3a2f07b
MiltonBarraza <miltonbarraza90@gmail.comgit config --global user.email miltonbarraza90@gmail.com>
2022-09-12 18:26:17 -0300

    Plantilla 5

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.scss';
 import Layout from '../components/layout';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <meta name="viewport" content='width=widthDevice initial-scale=1.0'/>
+    </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   )
 }
 
